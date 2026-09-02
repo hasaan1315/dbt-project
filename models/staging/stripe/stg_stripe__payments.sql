eb87debc0f1d2698f6218c0d5db998e95bdcf0ac
Muhammad Hasaan <hassaan70306@gmail.com>
2026-09-02 07:54:56 +0000

    Dpt all things added

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,11 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount,
+    created as created_at
+
+from {{ source('stripe', 'payment') }}
+
+where status <> 'fail'
